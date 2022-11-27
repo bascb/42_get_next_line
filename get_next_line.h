f10c81f2ddc95d176cd59874f2955515e210b37f
Bernardo Castelo Branco <bcastelo@c1r3s1.42lisboa.com>
2022-11-27 16:53:33 +0000

    First files created

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/27 16:48:40 by bcastelo          #+#    #+#             */
+/*   Updated: 2022/11/27 16:51:54 by bcastelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
